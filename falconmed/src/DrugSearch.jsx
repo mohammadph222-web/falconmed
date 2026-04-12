17c1de04209731ccd2c36aff66b314f204d433ed
mohammadph222-web <mohammad.ph.222@gmail.com>
2026-04-12 13:18:40 +0400

    Restore DrugSearch component

---
+++ falconmed/src/DrugSearch.jsx
@@ -0,0 +1,10 @@
+export default function DrugSearch() {
+  return (
+    <div style={{ padding: "24px" }}>
+      <h1 style={{ margin: 0, fontSize: "24px", fontWeight: 700 }}>Drug Search</h1>
+      <p style={{ marginTop: "8px", color: "#666" }}>
+        Drug Search page is loading correctly.
+      </p>
+    </div>
+  );
+}
